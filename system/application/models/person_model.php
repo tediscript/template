8c6f6ebc9199eb091007d82844f02540b50ed62c
tediscript <tediscript@gmail.com>
2010-10-18 19:03:37 +0700

    add person model. a generic model for all table

---
+++ system/application/models/person_model.php
@@ -0,0 +1,56 @@
+<?php
+
+class Person_model extends Model {
+
+    var $table_name = 'kategori';
+
+    function Person_model() {
+        parent::Model();
+        $this->load->database();
+    }
+
+    function save($data) {
+        return $this->db->insert($this->table_name, $data);
+    }
+
+    function remove($data_query) {
+        foreach ($data_query as $key => $value) {
+            $this->db->where($key, $value);
+        }
+        return $this->db->delete($this->table_name);
+    }
+
+    function update($data_query, $data) {
+        foreach ($data_query as $key => $value) {
+            $this->db->where($key, $value);
+        }
+        return $this->db->update($this->table_name, $data);
+    }
+
+    function find($data_query, $limit = 0, $offset = 0) {
+        foreach ($data_query as $key => $value) {
+            $this->db->where($key, $value);
+        }
+        $result = array();
+        $query = $this->db->get($this->table_name);
+        foreach ($query->result_array() as $row) {
+            $result[] = $row;
+        }
+        return $result;
+    }
+
+    function find_one($data_query) {
+        foreach ($data_query as $key => $value) {
+            $this->db->where($key, $value);
+        }
+        $result = array();
+        $query = $this->db->get($this->table_name);
+        foreach ($query->result_array() as $row) {
+            $result = $row;
+        }
+        return $result;
+    }
+
+}
+
+?>
